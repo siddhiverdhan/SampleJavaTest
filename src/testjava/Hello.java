bd831baa341860edac942647f57a41bf2a852260
Sidhi Verdhan <sidhiverdhan@Sidhis-MBP.attlocal.net>
2020-05-31 23:57:16 -0500

    first commit

---
+++ src/testjava/Hello.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("HelloWord");
+		HashMap<String, String> test = new HashMap<String, String>();
+		test.put("One:","1");
+		test.put("Two:","2");
+		System.out.println(test);
+	
+}
+
+}
+
---
+++ src/testjava/Test.java
@@ -0,0 +1,18 @@
+package testjava;
+
+import java.util.HashMap;
+
+public class Test {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello word");
+		HashMap<String, String> test = new HashMap<String, String>();
+		test.put("one", "1");
+		test.put("two", "2");
+		System.out.println(test);
+
+	}
+
+}
